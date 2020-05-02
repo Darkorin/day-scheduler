7ac4929be45d5f1c0f17957899c2c5f6616f5b05
David Stewart <darkorin00@hotmail.com>
2020-05-01 20:23:17 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # day-scheduler
+
+A simple day scheduler I did for homework. 
+It checks the current date and sets the date area to it.
+It then builds a table for all the business hours (9AM - 5PM)
+and color codes them based on if they're past, present, or future.
+It checks the local storage to see if there is any saved descriptions
+and if there is, it loads them into the correct text area. 
+The user may save new entries with the save button on the right side
+of each row.
---
+++ script.js
@@ -0,0 +1,62 @@
+
+
+let schedule = {
+    9: "",
+    10: "",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: "",
+}
+let tempStr = localStorage.getItem("schedule");
+console.log(tempStr);
+if(tempStr != undefined) {
+    schedule = JSON.parse(tempStr);
+}
+const renderDate = function() {
+    let date = moment().format("dddd, MMMM Do");
+    $("#currentDay").text(date);
+}
+
+
+const renderTable = function () {
+    let currentHour = moment().hour();
+    for (let i = 9; i < 18; i++) {
+        let newBlock = $("<div>").attr("class", "row")
+        if (i < 12) {
+            newBlock.append($("<div>").attr("class", "col hour").text(`${i}AM`));
+        } else if (i === 12) {
+            newBlock.append($("<div>").attr("class", "col hour").text(`${i}PM`));
+        } else {
+            newBlock.append($("<div>").attr("class", "col hour").text(`${i - 12}PM`));
+        }
+        let blockDesc = $("<textarea>").attr("id", `desc${i}`);
+        if (currentHour > i) {
+            blockDesc.attr("class", "col-10 description past");
+        } else if (currentHour === i) {
+            blockDesc.attr("class", "col-10 description present");
+        } else {
+            blockDesc.attr("class", "col-10 description future");
+        }
+        blockDesc.text(schedule[i]);
+        newBlock.append(blockDesc);
+        newBlock.append($("<button>").attr("class", "col saveBtn").attr("data-time", i).append($("<img>").attr("src", "assets/saveBtn.png")));
+        $(".time-block").append($(newBlock))
+    }
+}
+
+const clickHandler = function(e) {
+    e.preventDefault();
+    let time = this.dataset.time;
+    schedule[time] = $(`#desc${time}`).val();
+    console.log(schedule[time]);
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+
+renderDate();
+renderTable();
+$(".saveBtn").on("click", clickHandler);
